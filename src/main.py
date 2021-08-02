c869ba1b1301bfc02c0d6d8939497748d641ee9d
Tyler Beringer <99tylerberinger@gmail.com>
2021-08-02 12:54:08 -0400

    Added SQLAlchemy database with models User, TodoList

--- src/main.py
+++ src/main.py
@@ -3,12 +3,31 @@ import os
 from flask import Flask, render_template, url_for
 
 from flask_wtf import FlaskForm
+
+from flask_sqlalchemy import SQLAlchemy
+
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.validators import Email, DataRequired
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(32).hex()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+database = SQLAlchemy(app)
+
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    email = database.Column(database.String, nullable=False, unique=True)
+    password = database.Column(database.String, nullable=False)
+    picture = database.Column(database.String, nullable=False, default="picture_default.png")
+    todo_list = database.relationship("TodoList", backref="user")
+
+class TodoList(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    # ...
+    user_id = database.Column(database.Integer, database.ForeignKey("user.id"), nullable=False)
+
 
 # POST
 class FormRegister(FlaskForm):
